fe66efbdbe5c0aedb4a8e30e2785203fcf229143
Ville Verkkonen <villeverkkonen@gmail.com>
2022-10-08 20:08:45 +0300

    Serving static json instead fetching API every load

--- src/main/java/solid/abis/challenge/controller/FoodController.java
+++ src/main/java/solid/abis/challenge/controller/FoodController.java
@@ -1,6 +1,5 @@
 package solid.abis.challenge.controller;
 
-import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -8,52 +7,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Scanner;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/foods")
 public class FoodController {
 
-    private URL url = new URL("https://fineli.fi/fineli/api/v1/components");
-
-    public FoodController() throws MalformedURLException {
-    }
+    private static final String FOOD_JSON = "json/foods.json";
 
     @GetMapping("")
     public String foods() {
-        try {
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            int resCode = conn.getResponseCode();
-            if (resCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + resCode);
-            } else {
-                String inline = "";
-                Scanner scanner = new Scanner(url.openStream());
-
-                while (scanner.hasNext()) {
-                    inline += scanner.nextLine();
-                }
-
-                scanner.close();
-
-                JSONParser parse = new JSONParser();
-                JSONArray dataObj = (JSONArray) parse.parse(inline);
-
-                System.out.println(dataObj.toString());
-
-                return dataObj.toString();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
+        FoodController app = new FoodController();
+        try (FileReader reader = new FileReader(app.getFileFromResource(FOOD_JSON))) {
+            Object obj = new JSONParser().parse(reader);
+            JSONArray foods = (JSONArray) obj;
+            return foods.toString();
+        } catch (IOException | ParseException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+
     }
 }
