d6ba68e7030c5e4768ec3d63eab9b46c6dc3462d
Ville Verkkonen <villeverkkonen@gmail.com>
2022-10-08 22:42:50 +0300

    Refactored logic from controller to service class

--- src/main/java/solid/abis/challenge/controller/FoodController.java
+++ src/main/java/solid/abis/challenge/controller/FoodController.java
@@ -1,55 +1,23 @@
 package solid.abis.challenge.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solid.abis.challenge.dto.FoodDTO;
-import solid.abis.challenge.mapper.FoodMapper;
+import solid.abis.challenge.service.FoodService;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/foods")
 public class FoodController {
 
     @Resource
-    private FoodMapper foodMapper;
-
-    private static final String FOOD_JSON = "json/foods.json";
+    FoodService foodService;
 
     @GetMapping("")
     public List<FoodDTO> getFoods() {
-        try (FileReader reader = new FileReader(this.getFileFromResource(FOOD_JSON))) {
-            Object obj = new JSONParser().parse(reader);
-            JSONArray foods = (JSONArray) obj;
-            return foodMapper.jsonArrayToDtoList(foods);
-        } catch (IOException | ParseException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private File getFileFromResource(final String fileName) throws URISyntaxException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("File not found! " + fileName);
-        } else {
-            return new File(resource.toURI());
-        }
-
+        return foodService.getFoodsFromJson();
     }
 }
---
+++ src/main/java/solid/abis/challenge/service/FoodService.java
@@ -0,0 +1,45 @@
+package solid.abis.challenge.service;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+import solid.abis.challenge.dto.FoodDTO;
+import solid.abis.challenge.mapper.FoodMapper;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+@Service
+public class FoodService {
+
+    @Resource
+    private FoodMapper foodMapper;
+
+    private static final String FOOD_JSON = "json/foods.json";
+
+    public List<FoodDTO> getFoodsFromJson() {
+        try (FileReader reader = new FileReader(this.getFileFromResource(FOOD_JSON))) {
+            Object obj = new JSONParser().parse(reader);
+            JSONArray foods = (JSONArray) obj;
+            return foodMapper.jsonArrayToDtoList(foods);
+        } catch (IOException | ParseException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File getFileFromResource(final String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+}
