b8dd78bac6551cd80bf4527e025e8562f9027a65
Ville Verkkonen <villeverkkonen@gmail.com>
2022-10-08 17:40:42 +0300

    Data from backend to UI

--- src/vue-frontend/src/App.vue
+++ src/vue-frontend/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CodeChallenge />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CodeChallenge from './components/CodeChallenge.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CodeChallenge
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/vue-frontend/src/components/CodeChallenge.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="code-challenge">
+    <h1>Food Fight</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodeChallenge',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/api/challenge/hello")
+    .then((response) => response.text())
+    .then((data) => {
+      this.msg = data;
+    });
+  }
+}
+</script>
+
